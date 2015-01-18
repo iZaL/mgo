e891299996dc88f2d78660d643e50c1b1a44b3c4
unknown <usama@usama-PC.EPIC.COM.KW>
2015-01-18 08:00:26 +0300

    some modification and corrections in the CSS layout

--- app/views/site/blog/search.blade.php
+++ app/views/site/blog/search.blade.php
@@ -3,9 +3,17 @@
         {{ trans('word.search') }}
     </div>
     <div class="panel-body">
-        {{ Form::open(['action' => 'BlogsController@index', 'method' => 'get'], ['class'=>'form']) }}
-            {{ Form::text('search', null, ['class'=>'form-control', 'placeholder'=>trans('word.search')]) }}
-            <button type="submit" class="btn btn-default">{{ trans('word.search') }}</button>
-        {{Form::close()}}
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-9">
+            {{ Form::open(['action' => 'BlogsController@index', 'method' => 'get'], ['class'=>'form']) }}
+                {{ Form::text('search', null, ['class'=>'form-control', 'placeholder'=>trans('word.search')]) }}
+                </div>
+                <div class="col-md-3">
+                <button type="submit" class="btn btn-primary">{{ trans('word.search') }}</button>
+            {{Form::close()}}
+                </div>
+            </div>
+        </div>
     </div>
 </div>
