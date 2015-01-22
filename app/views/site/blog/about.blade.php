27130d7b1b04767f303d85329d63cb58ee5e4481
ZaL <z4ls@live.com>
2015-01-22 07:48:41 +0300

    change layout

--- app/views/site/blog/about.blade.php
+++ app/views/site/blog/about.blade.php
@@ -4,18 +4,6 @@
     <div class="col-md-12">
         <div class="col-md-8 col-md-offset-2">
             <div class="row">
-                <div class="well well-sm" style="margin-bottom: 10px;">
-                        <span class="label label-default
-                        @if ( App::getLocale() == 'en')
-                            pull-right
-                        @else
-                                pull-left
-                            @endif
-                                " style=" padding: 5px; margin:0px; margin-bottom: 5px;">
-                        Posted {{ $post->created_at }}
-                        </span>
-                </div>
-
                 <p class="text-justify">{{ $post->description }}</p>
             </div>
         </div>
