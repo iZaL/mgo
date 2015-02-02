d8ef6a6fabf2ff7b06b434912b1df158cd05c54d
ZaL <z4ls@live.com>
2015-02-02 17:29:46 +0300

    some css changes

--- app/views/site/home.blade.php
+++ app/views/site/home.blade.php
@@ -63,10 +63,8 @@
             <!-- Content Section -->
 @section('content')
     @parent
-    {{--@include('site.galleries.slider')--}}
     <div class="col-md-12">
         <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 600px; height: 350px; overflow: hidden; ">
-            <!-- Loading Screen -->
             <div u="loading" style="position: absolute; top: 0px; left: 0px;">
                 <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
                 background-color: #000000; top: 0px; left: 0px;width: 100%;height:100%;">
@@ -76,10 +74,8 @@
                 </div>
             </div>
 
-            <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 350px; overflow: hidden;">
-                {{--@foreach($galleries as $gallery)--}}
-                @if($gallery && count($gallery->photos))
-                    <!-- Slides Container -->
+            @if($gallery && count($gallery->photos))
+                <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 350px; overflow: hidden;">
                     @foreach($gallery->photos as $photo)
                         <div>
                             <a u="image" href="{{action('GalleriesController@show',$gallery->id)}}">
@@ -91,28 +87,21 @@
                             </a>
                         </div>
                     @endforeach
-                @endif
-                {{--@endforeach--}}
-            </div>
-            <!-- bullet navigator container -->
+
+                    <div class="description">
+                        {{ $gallery->description }}
+                    </div>
+                </div>
+            @endif
+
             <div u="navigator" class="jssorb05" style="position: absolute; bottom: 16px; right: 6px; ">
-                <!-- bullet navigator item prototype -->
                 <div u="prototype" style="POSITION: absolute; WIDTH: 16px; HEIGHT: 16px;"></div>
             </div>
-            <!-- Bullet Navigator Skin End -->
-            <!-- Arrow Navigator Skin Begin -->
-
-            <!-- Arrow Left -->
-        <span u="arrowleft" class="jssora12l" style="width: 30px; height: 46px; top: 123px; left: 0px;">
-        </span>
-            <!-- Arrow Right -->
-        <span u="arrowright" class="jssora12r" style="width: 30px; height: 46px; top: 123px; right: 0px">
-        </span>
-            <!-- Arrow Navigator Skin End -->
+            <span u="arrowleft" class="jssora12l" style="width: 30px; height: 46px; top: 123px; left: 0px;"></span>
+            <span u="arrowright" class="jssora12r" style="width: 30px; height: 46px; top: 123px; right: 0px"></span>
             <a style="display: none" href="http://www.jssor.com">Image Slider</a>
         </div>
     </div>
-
 
 @stop
 
